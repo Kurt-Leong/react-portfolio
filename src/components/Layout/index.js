2b6e8cf96a9feafb1e5df1f4bddd49f35257e6df
Kurt Liang <liangke1991@gmail.com>
2023-04-28 17:37:27 +1200

    home page html

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,22 @@
-import Sidebar from '../Sidebar'
+import { Outlet } from 'react-router-dom'
+import NavBar from '../NavBar'
 import './index.scss'
 
 export default function Layout() {
   return (
     <>
-      <Sidebar />
+      <div className='"App"'>
+        <NavBar />
+        <div className="page">
+          <span className="tags top-tags">&lt;body&gt;</span>
+          <Outlet />
+          <span className="tags bottom-tags">
+            &lt;body&gt;
+            <br />
+            <span className="bottom-tag-html"> &lt;html&gt;</span>
+          </span>
+        </div>
+      </div>
     </>
   )
 }
