fa5178a6ee20a546b3a7c3f6584d1a3446c040bd
Kurt Liang <liangke1991@gmail.com>
2023-05-01 13:23:45 +1200

    make letter in the homepage dynanmic

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,45 @@
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-k.png'
 import './index.scss'
+import { useEffect, useState } from 'react'
+import AnumatedLetters from '../AnimatedLetters'
 
 export default function Home() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  function splitWord(str) {
+    return str.split('')
+  }
+  const nameArray = splitWord('urt Liang')
+  const jobArray = splitWord('web developer.')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
   return (
     <>
       <div className="container home-page">
         <div className="text-zone ">
           <h1>
-            Hi,
-            <br /> I'm &nbsp; <img src={LogoTitle} alt="developer" />
-            urt Liang
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
             <br />
-            Web developer
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            &nbsp; <img src={LogoTitle} alt="developer" />
+            <AnumatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              index={15}
+            />
+            <br />
+            <AnumatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              index={24}
+            />
           </h1>
           <h2> Full Stack Developer</h2>
           <Link to="/contact" className="flat-button">
